da224059e621a1d6c186c8126f6dd07a5711293a
yuuki1036 <yuuki1036@gmail.com>
2025-01-19 20:51:18 +0900

    fix: build error

--- src/app/[lang]/page.tsx
+++
@@ -1,9 +0,0 @@
-// app/[lang]/page.tsx
-export default function Page({ params }: { params: { lang: string } }) {
-  return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>yasu224</h1>
-      <h2>Current language: {params.lang}</h2>
-    </main>
-  );
-}
