31159faf215abc59cd73c04f2009b7aef8cb34cb
yuuki1036 <yuuki1036@gmail.com>
2025-06-08 15:09:22 +0900

    chore: init storybook

---
+++ .storybook/preview.ts
@@ -0,0 +1,35 @@
+import type { Preview } from '@storybook/nextjs'
+import '../src/app/globals.css'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+       color: /(background|color)$/i,
+       date: /Date$/i,
+      },
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  globalTypes: {
+    locale: {
+      description: 'Internationalization locale',
+      toolbar: {
+        title: 'Language',
+        icon: 'globe',
+        items: [
+          { value: 'ja', title: '🇯🇵 日本語' },
+          { value: 'en', title: '🇺🇸 English' },
+        ],
+        dynamicTitle: true,
+      },
+    },
+  },
+  initialGlobals: {
+    locale: 'ja',
+  },
+};
+
+export default preview;
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import { dirname } from "node:path";
 import { fileURLToPath } from "node:url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,6 +14,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...storybook.configs["flat/recommended"]
 ];
 
 export default eslintConfig;
